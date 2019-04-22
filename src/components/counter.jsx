af875f7a3b7795ab05a369b179deb2c266c80265
Michael Dorst <m@mdorst.net>
2019-04-21 23:46:40 -0700

    Add onclick method

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,13 +5,22 @@ class Counter extends Component {
     count: 0
   }
 
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 })
+  }
+
   render() {
     return (
       <div>
         <span className={this.counterClasses()}>
           {this.state.count === 0 ? "Zero" : this.state.count}
         </span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
       </div>
     )
   }
