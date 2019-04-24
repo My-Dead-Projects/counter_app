bbb8a1dfe4549ee87cb8e4f3b9f3d561dddf7ccf
Michael Dorst <m@mdorst.net>
2019-04-24 00:44:00 -0700

    Functionalize components

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,33 +1,27 @@
-import React, { Component } from "react"
+import React from "react"
 
-class Counter extends Component {
-  render() {
-    return (
-      <div>
-        <span className={this.counterClasses()}>
-          {this.props.counter.value === 0 ? "Zero" : this.props.counter.value}
-        </span>
-        <button
-          onClick={() => this.props.onIncrement(this.props.counter)}
-          className="btn btn-secondary btn-sm"
-        >
-          +
-        </button>
-        <button
-          onClick={() => this.props.onDelete(this.props.counter.id)}
-          className="btn btn-danger btn-sm"
-        >
-          Delete
-        </button>
-      </div>
-    )
-  }
-
-  counterClasses() {
-    return `badge m-2 badge-${
-      this.props.counter.value === 0 ? "warning" : "primary"
-    }`
-  }
-}
+const Counter = ({ counter, onIncrement, onDelete }) => (
+  <div>
+    <span
+      className={`badge m-2 badge-${
+        counter.value === 0 ? "warning" : "primary"
+      }`}
+    >
+      {counter.value}
+    </span>
+    <button
+      onClick={() => onIncrement(counter)}
+      className="btn btn-secondary btn-sm m-2"
+    >
+      +
+    </button>
+    <button
+      onClick={() => onDelete(counter.id)}
+      className="btn btn-danger btn-sm"
+    >
+      Delete
+    </button>
+  </div>
+)
 
 export default Counter
